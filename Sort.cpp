4f2a1add15eb39ba6e5a62fe13443a32d7baba28
MarianaPalos <42983049+MarianaPalos@users.noreply.github.com>
2018-09-20 15:32:53 -0500

    Add files via upload

--- Sort.cpp
+++ Sort.cpp
@@ -4,25 +4,30 @@ using namespace std;
 
 void Sort (int Lista[10])
 {
-int i,j,temp=0;
+int i,j,temp=0, cont1=0, cont2=0, cont3=0;
 for(i=0;i<10;i++)
  {
-  for(j=0;j<10;j++)
-   {
-     if(Lista[i]<Lista[j])
+  for(j=i;j<10;j++)
+   { cont1++;
+     if(Lista[i]>Lista[j])
     {
       temp=Lista[i];
       Lista[i]=Lista[j];
       Lista[j]=temp;
+      cont2++;
     }
    }
-  }
+ } cont3++;
 
 cout << "La Lista de Numeros ordenada es:\n";
 for(i=0;i<10;i++)
  {
  cout<<" "<<Lista[i]<<endl;
  }
+//cout <<cont1<<endl;
+//cout<<cont2<<endl;
+//cout<<cont3<<endl;
+cout << "Big O: " << cont1+cont2 << endl;
 }
 
 
--- SortPunt.cpp
+++ SortPunt.cpp
@@ -4,25 +4,32 @@ using namespace std;
 
 void SortPunt (int *p)
 {
-int i,j,temp;
+int i,j,temp, contador=0, contador1=0, contador2=0;
 for(i=0;i<10;i++)
  {
-  for(j=0;j<10;j++)
-   {
-     if(*(p+i)<*(p+j))
+  for(j=i;j<10;j++)
+   { contador++;
+     if(*(p+i)>*(p+j))
     {
       temp=*(p+i);
       *(p+i)=*(p+j);
       *(p+j)=temp;
+      contador1++;
     }
-   }
   }
+
+} contador2++;
 
 cout << "La Lista de Numeros ordenada es:\n";
 for(i=0;i<10;i++)
  {
  cout<<" "<<*(p+i)<<endl;
  }
+
+//cout << contador<< endl;
+//cout << contador1<< endl;
+//cout << contador2<< endl;
+cout << "Big O: " << contador+contador1 << endl;
 }
 
 
